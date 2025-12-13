0e54d7c36deef590a583fa45bc953f2981f3a33a
ankit <ankithss774@gmail.com>
2025-12-14 02:54:55 +0530

    added some utilities to signals

--- appmanager/signals.py
+++ appmanager/signals.py
@@ -1,12 +1,23 @@
 import os
 import zipfile
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.conf import settings
+from django.template import engines
 from .models import App
 
 @receiver(post_save, sender=App)
 def handle_build_artifact(sender, instance, created, **kwargs):
+    # Clear template cache for this app
+    django_engine = engines['django']
+    template_name = f'apps/{instance.name}/index.html'
+    if hasattr(django_engine, 'engine') and hasattr(django_engine.engine, 'template_loaders'):
+        # For Django < 3.2
+        django_engine.engine.template_loaders = []
+    if hasattr(django_engine, 'template_loaders'):
+        # For Django >= 3.2
+        django_engine.template_loaders = []
+
     if created and instance.build_file:
         # Define paths for templates and static
         templates_dir = os.path.join(settings.BASE_DIR, 'templates', 'apps', instance.name)
@@ -25,3 +36,23 @@ def handle_build_artifact(sender, instance, created, **kwargs):
         index_html_path = os.path.join(static_dir, 'index.html')
         if os.path.exists(index_html_path):
             os.rename(index_html_path, os.path.join(templates_dir, 'index.html'))
+
+@receiver(post_delete, sender=App)
+def remove_build_artifact(sender, instance, **kwargs):
+    # Remove templates directory
+    templates_dir = os.path.join(settings.BASE_DIR, 'templates', 'apps', instance.name)
+    static_dir = os.path.join(settings.STATIC_PATH_INSTALLED_APPS, instance.name)
+
+    if os.path.exists(templates_dir):
+        try:
+            import shutil
+            shutil.rmtree(templates_dir)
+        except Exception:
+            pass
+
+    if os.path.exists(static_dir):
+        try:
+            import shutil
+            shutil.rmtree(static_dir)
+        except Exception:
+            pass
