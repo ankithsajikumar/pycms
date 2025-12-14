b884119edb1977c6e172ce1a8f7c8602ebe60287
ankit <ankithss774@gmail.com>
2025-12-14 18:59:56 +0530

    App user assignment update

--- appmanager/admin.py
+++ appmanager/admin.py
@@ -4,4 +4,9 @@ from .models import App
 @admin.register(App)
 class AppAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_at')
-    fields = ('user', 'name', 'repo_url', 'subdomain', 'status', 'build_file')
+    fields = ('name', 'repo_url', 'subdomain', 'status', 'build_file')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
